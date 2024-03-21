22d4196ddbacb658143914adf79f9c91401fa999
hilmyyes <irfansahilmy@gmail.com>
2024-03-21 14:06:30 +0700

    materi dosen (done)
    gambar ngga muncul, nanti diperbaiki

--- app/src/main/java/com/example/Adv160421080week4/model/Model.kt
+++ app/src/main/java/com/example/Adv160421080week4/model/Model.kt
@@ -4,8 +4,11 @@ import com.google.gson.annotations.SerializedName
 
 data class Student(
     val id:String?,
+    @SerializedName("student_nama")
     val name:String?,
+    @SerializedName("birth_of_date")
     val dob:String?,
     val phone:String?,
+    @SerializedName("photo_url")
     val photoUrl:String?,
 )
